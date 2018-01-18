b1b35bff9ab71a4f6faa7cc512986c9547af8a80
Gianluca Giorgolo <gianluca.giorgolo@gmail.com>
2018-01-18 16:00:59 +0100

    forgot to remove the local settings for waitress

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def root():
 
 
 if __name__ == "__main__":
-    # serve(app, host='0.0.0.0', port=80, channel_timeout=120)
-    serve(app, host='127.0.0.1', port=4000, channel_timeout=120)
+    serve(app, host='0.0.0.0', port=80, channel_timeout=120)
+    # serve(app, host='127.0.0.1', port=4000, channel_timeout=120)
